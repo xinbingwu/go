14cc739e9b07ee1971ac634501bcb2efc9addb30
Your Name <you@example.com>
2019-05-29 22:08:40 +0800

    first commit

---
+++ 1.go
@@ -0,0 +1,14 @@
+package main
+import (  //这里必须是小括号，而不能是大括号
+	"fmt"  //这里必须是双引号
+ 	"math"
+)
+
+func main(){   //这个大括号一定不能换行
+	fmt.Printf("%f\n", math.Pi)
+        fmt.Printf("%.2f\n", math.Pi)
+
+}
+
+
+
---
+++ 3.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("hello world")  //这里是小写的L而不是i
+}
